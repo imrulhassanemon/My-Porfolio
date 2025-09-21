b35035697b1fae9e4b1e47d76ce9d1fca4730088
imrulhassanemon <imrulhassan989@gmail.com>
2025-09-21 12:35:21 -0700

    project setup for my Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './section/Navbar'
+
+function App() {
+
+  return (
+
+      <div className='z mx-auto max-w-7xl bg-yellow border border-red-500'>
+        <Navbar></Navbar>
+      </div>
+  )
+}
+
+export default App
---
+++ src/section/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='fixed inset-x-0 z-10 w-full backdrop:blur-lg border border-amber-300 '>
+            <div className='flex items-center justify-between py-2 sm:py-0'>
+                <a className='text-xl font-bold transition-colors text-neutral-400 hover:text-white'>Lorem, ipsum dolor.</a>
+            </div>
+    </div>
+  )
+}
