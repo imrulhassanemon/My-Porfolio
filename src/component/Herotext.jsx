aba1e8a2da08d2336ffbe1b7c41a6e3a444f22db
imrulhassanemon <imrulhassan989@gmail.com>
2025-09-27 01:42:18 -0700

    added background

--- src/component/Herotext.jsx
+++ src/component/Herotext.jsx
@@ -1,24 +1,79 @@
 import { FlipWords } from "./FlipWords"
+import { motion } from "motion/react"
 
 function Herotext() {
+
+    const words = ["Secure", "Modern","Sclable"]
+    const varients = {
+        hidden:{opacity:0, x: -50},
+        visible:{opacity:1, x:0}
+        
+    }
+
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
         {/* mobail view  */}
         <div className="flex-col hidden md:flex c-space">
-            <h1 className="text-4xl font-medium">Hi || I am Imrul Hassan Emon</h1>
+            <motion.h1 className="text-4xl font-medium"
+            variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1}}
+            >Hi || I am Imrul Hassan Emon</motion.h1>
             <div className="flex flex-col items-start">
-                <p className="text-neutral-300 text-5xl font-medium">A Developer <br /> Dedicate to Crafting</p>
-                <div>
-                    <FlipWords words={["Secure", "Modern","Sclable"]} className='text-white text-8xl font-black' ></FlipWords>
-                </div>
-                <p className="text-4xl font-medium text-neutral-300">Web soutions</p>
+                <motion.p className="text-neutral-300 text-5xl font-medium"
+                variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1.2}}
+                >A Developer <br /> Dedicate to Crafting</motion.p>
+                <motion.div
+                variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1.5}}
+                >
+                    <FlipWords words={words} className='text-white text-8xl font-black' ></FlipWords>
+                </motion.div>
+                <motion.p
+                variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1.8}}
+                className="text-4xl font-medium text-neutral-300">Web soutions</motion.p>
             </div>
         </div>
         
         {/* desktop view  */}
 
-        <div>
-            <p className="text-4xl font-medium"> HI I am Imrul</p>
+        <div className="flex flex-col space-y-6 md:hidden">
+            <motion.p
+            variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1}}
+            className="text-4xl font-medium"> HI I am Imrul</motion.p>
+            <div>
+                <motion.p className="text-5xl font-black text-neutral-300"
+                variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1.2}}
+                >Building</motion.p>
+                <motion.div 
+                variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1.5}}
+                >
+                    <FlipWords words={words} className='font-bold text-white text-7xl' ></FlipWords>
+                </motion.div>
+                <motion.p className="text-4xl font-black text-neutral-300"
+                variants={varients}
+            initial='hidden'
+            animate="visible"
+            transition={{delay:1.8}}>Web Application</motion.p>
+            </div>
         </div>
 
     </div>
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Herotext from '../component/Herotext';
+import ParallalBackground from '../component/ParallalBackground';
 
 const Hero = () => {
     return (
         <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
             <Herotext></Herotext>
+            <ParallalBackground></ParallalBackground>
         </section>
     );
 };
