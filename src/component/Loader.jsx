e7780db141c3dc442b8d9d6166466f00561e938c
imrulhassanemon <imrulhassan989@gmail.com>
2025-10-06 03:02:07 -0700

    added animation to 3d object

---
+++ src/component/Loader.jsx
@@ -0,0 +1,11 @@
+import { Html, useProgress } from "@react-three/drei";
+import React from "react";
+
+export const Loader = () => {
+
+    const progress = useProgress()
+
+  return <Html center>
+            {progress} %Loaded
+  </Html>;
+};
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Herotext from '../component/Herotext';
 import ParallalBackground from '../component/ParallalBackground';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { Astronut } from '../component/Astronut';
-import { OrbitControls } from '@react-three/drei';
+import { Float,  OrbitControls } from '@react-three/drei';
+import { useMediaQuery } from 'react-responsive';
+import { easing } from 'maath';
+import { Loader } from '../component/Loader';
 
 
 const Hero = () => {
+
+    const isMobail = useMediaQuery({maxWidth: 853})
+
     return (
         <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
             <Herotext></Herotext>
             <ParallalBackground></ParallalBackground>
-            <figure className='absolute inset-0 w-screen h-screen z-0'>
-            <Canvas>
-               <Astronut></Astronut>
+            <figure className='absolute inset-0 w-screen h-screen z-40' style={{width:'100vw', height:'100vh'}}>
+            <Canvas camera={{position:[0, 1, 3]}}>
+               <Suspense fallback={<Loader></Loader>}>
+                <Float>
+                <Astronut scale={isMobail && 0.23} position={isMobail && [0, -1.5, 0]} />
+               </Float>
                <OrbitControls></OrbitControls>
+               <Rig></Rig>
+               </Suspense>
             </Canvas>     
             </figure>
         </section>
     );
 };
 
+function Rig() {
+  return useFrame((state, delta) =>{
+    easing.damp3(
+      state.camera.position,[state.mouse.x/10, 1 + state.mouse.y/10, 3],0.5,delta
+    )
+  })
+}
 export default Hero;
